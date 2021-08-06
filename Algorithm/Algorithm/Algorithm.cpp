bd7934b7242a7d546f46ec564c34f6bf6526e938
password-kim <72890183+password-kim@users.noreply.github.com>
2021-08-06 18:56:47 +0900

    Make List

--- Algorithm/Algorithm/Algorithm.cpp
+++ Algorithm/Algorithm/Algorithm.cpp
@@ -1,7 +1,9 @@
 ﻿#include <iostream>
 #include <vector>
+#include <list>
 using namespace std;
 
+#pragma region Vector
 template<typename T>
 class Vector
 {
@@ -100,34 +102,219 @@ private:
     int _size = 0;
     int _capacity = 0;
 };
+#pragma endregion
+
+#pragma region List
+template<typename T>
+class Node
+{
+public:
+    Node() : _prev(nullptr), _next(nullptr), _data(T())
+    {
+
+    }
+
+    Node(const T& value) : _prev(nullptr), _next(nullptr), _data(value)
+    {
+
+    }
+    
+public:
+    Node*   _prev;
+    Node*   _next;
+    T       _data;
+
+
+};
+
+template<typename T>
+class Iterator
+{
+public:
+    Iterator() : _node(nullptr)
+    {
+
+    }
+
+    Iterator(Node<T>* node) : _node(node)
+    {
+
+    }
+
+    // ++it
+    Iterator& operator++()
+    {
+        _node = _node->_next;
+        return _node;
+    }
+
+    // it++
+    Iterator& operator++(int)
+    {
+        Iterator<T> temp = *this;
+        _node = _node->_next;
+        return temp;
+    }
+
+    // --it
+    Iterator& operator--()
+    {
+        _node = _node->_prev;
+        return _node;
+    }
+
+    // it--
+    Iterator& operator--(int)
+    {
+        Iterator<T> temp = *this;
+        _node = _node->_prev;
+        return temp;
+    }
+
+    // *it
+    T& operator*()
+    {
+        return _node->_data;
+    }
+
+    bool operator==(const Iterator& other)
+    {
+        return _node == other._node;
+    }
+
+    bool operator!=(const Iterator& other)
+    {
+        return _node != other._node;
+    }
+
+public:
+    Node<T>* _node;
+};
+
+template<typename T>
+class List
+{
+public:
+    List() : _size(0)
+    {
+        // [head] <=> [tail]
+        _head = new Node<T>();
+        _tail = new Node<T>();
+        _head->_next = _tail;
+        _tail->_prev = _head;
+    }
+
+    ~List()
+    {
+        while (_size > 0)
+            pop_back();
+
+        delete _head;
+        delete _tail;
+    }
+
+    void push_back(const T& value)
+    {
+        AddNode(_tail, value);
+    }
+
+    void pop_back()
+    {
+        RemoveNode(_tail->_prev);
+    }
+
+private:
+
+    // [head] <=> [1] <=> [2] <=> [before] <=> [tail]
+    // [head] <=> [1] <=> [2] <=> [newNode] <=> [before] <=> [tail]
+    Node<T>* AddNode(Node<T>* before, const T& value)
+    {
+        Node<T>* newNode = new Node<T>(value);
+        Node<T>* prevNode = before->_prev;
+
+        prevNode->_next = newNode;
+        newNode->_prev = prevNode;
+
+        newNode->_next = before;
+        before->_prev = newNode;
+
+        _size++;
+
+        return newNode;
+    }
+
+    // [head] <=> [prevNode] <=> [node] <=> [nextNode] <=> [tail]
+    // [head] <=> [prevNode] <=> [nextNode] <=> [tail]
+    Node<T>* RemoveNode(Node<T>* node)
+    {
+        Node<T>* prevNode = node->_prev;
+        Node<T>* nextNode = node->_next;
+
+        prevNode->_next = nextNode;
+        nextNode->_prev = prevNode;
+
+        delete node;
+
+        _size--;
+
+        return nextNode;
+    }
+
+
+    int size() { return _size; }
+
+
+public:
+    using iterator = Iterator<T>;
+
+    iterator begin() { return iterator(_head->_next); }
+    iterator end() { return iterator(_tail); }
+
+    // it '앞에' 추가
+    iterator insert(iterator it, const T& value)
+    {
+        Node<T>* node = AddNode(it._node, value);
+        return iterator(node);
+    }
+
+    iterator erase(iterator it)
+    {
+        Node<T>* node = RemoveNode(it._node);
+        return iterator(node);
+    }
+
+private:
+    Node<T>*    _head;
+    Node<T>*    _tail;
+    int         _size;
+};
+#pragma endregion
 
 int main()
 {
-    Vector<int> v;
+    List<int> li;
 
-    v.reserve(100); // default capacity를 100으로 설정.
+    List<int>::iterator eraseIt;
 
-    for (int i = 0; i < 100; i++) // v에 0부터 99까지의 숫자를 push_back하고 element와 size, capacity를 콘솔창에 출력.
+    for (int i = 0; i < 10; i++)
     {
-        v.push_back(i); 
-        cout << v[i] << " " << v.size() << " " << v.capacity() << endl;
+        if (i == 5)
+        {
+            eraseIt = li.insert(li.end(), i);
+        }
+        else
+        {
+            li.push_back(i);
+        }
     }
 
-    v.resize(10); // size를 10으로 resize.
+    li.pop_back();
 
-    for (int i = 0; i < v.size(); i++) // resize된 v의 element, size, capacity를 콘솔창에 출력.
+    li.erase(eraseIt);
+
+    for (auto it = li.begin(); it != li.end(); it++)
     {
-        cout << v[i] << " " << v.size() << " " << v.capacity() << endl;
+        cout << (*it) << endl;
     }
-
-    v.pop_back(); // v의 맨 뒤의 elements를 pop!
-
-    for (int i = 0; i < v.size(); i++) // 다시 v의 element, size, capacity를 콘솔창에 출력.
-    {
-        cout << v[i] << " " << v.size() << " " << v.capacity() << endl;
-    }
-    
-
-
 }
 
