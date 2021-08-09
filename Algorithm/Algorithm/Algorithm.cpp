281e779f98282af431c2576cea0c7258d8e379e8
password-kim <72890183+password-kim@users.noreply.github.com>
2021-08-09 14:53:29 +0900

    Make Queue!

--- Algorithm/Algorithm/Algorithm.cpp
+++ Algorithm/Algorithm/Algorithm.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <list>
 #include <stack>
+#include <queue>
 using namespace std;
 
 #pragma region Vector
@@ -323,28 +324,116 @@ private:
 
 #pragma endregion
 
-int main()
+#pragma region Queue
+
+// Queue (FIFO First-In-First-Out 선입선출)
+
+// front << [1][2][3][4] << rear(back)
+
+template<typename T>
+class ArrayQueue  // [][][front][][][back][][][][][]
 {
-    Stack<int> s;
-
-    // 삽입
-    s.push(1);
-    s.push(2);
-    s.push(3);
-
-    while (s.empty() == false)
+public:
+    ArrayQueue()
     {
-        // 최상위 원소
-        int data = s.top();
-
-        //최상위 원소 삭제
-        s.pop();
-
-        cout << data << endl;
+        _container.resize(100);
     }
 
+    void push(const T& value)
+    {
+        // TODO : 다 찼는지 체크
+        if (_size == _container.size())
+        {
+            // 증설 작업
+            int newSize = max(1, _size * 2);
+            vector<T> newData;
+            newData.resize(newSize);
 
-    int size = s.size();
-    
+            // 데이터 복사
+            for (int i = 0; i < _size; i++)
+            {
+                int index = (_front + i) % _container.size();
+                newData[i] = _container[index];
+            }
+
+            _container.swap(newData);
+            _front = 0;
+            _back = _size;
+        }
+
+        _container[_back] = value;
+        _back = (_back + 1) % _container.size();
+        _size++;
+    }
+
+    void pop()
+    {
+        _front = (_front + 1) % _container.size();
+        _size--;
+    }
+
+    T& front()
+    {
+        return _container[_front];
+    }
+
+    bool empty() { return _size == 0; }
+
+    int size() { return _container.size(); }
+
+private:
+    vector<T> _container;
+
+    int _front = 0;
+    int _back = 0;
+    int _size = 0;
+};
+
+template<typename T>
+class ListQueue
+{
+public:
+    void push(const T& value)
+    {
+        _container.push_back(value);
+    }
+
+    void pop()
+    {
+        _container.pop_front();
+    }
+
+    T& front()
+    {
+        return _container.front();
+    }
+
+    bool empty() { return _container.empty(); }
+
+    int size() { return _container.size(); }
+
+private:
+    list<T> _container;
+};
+
+#pragma endregion   
+
+int main()
+{
+    ArrayQueue<int> q;
+
+    for (int i = 0; i < 100; i++)
+        q.push(i);
+
+    while (q.empty() == false)
+    {
+        int value = q.front();
+        q.pop();
+        cout << value << endl;
+    }
+
+    int size = q.size();
+
+    cout << size << endl;
 }
 
