33abc15043d98057bd089357995dd33dca250c8f
password-kim <72890183+password-kim@users.noreply.github.com>
2021-08-10 15:32:36 +0900

    Basic Graph

--- Algorithm/Algorithm/Algorithm.cpp
+++ Algorithm/Algorithm/Algorithm.cpp
@@ -416,24 +416,138 @@ private:
     list<T> _container;
 };
 
-#pragma endregion   
+#pragma endregion  
+
+#pragma region Graph기초
+void CreateGraph1()
+{
+    struct Vertex
+    {
+        vector<Vertex*> edges;
+    };
+
+    vector<Vertex> v;
+    v.resize(6);
+
+    v[0].edges.push_back(&v[1]);
+    v[0].edges.push_back(&v[3]);
+    v[1].edges.push_back(&v[0]);
+    v[1].edges.push_back(&v[2]);
+    v[1].edges.push_back(&v[3]);
+    v[3].edges.push_back(&v[4]);
+    v[5].edges.push_back(&v[4]);
+
+    // 0번 -> 3번 정점이 연결되어 있는지 확인.
+    bool connected = false;
+    for (Vertex* edge : v[0].edges)
+    {
+        if (edge == &v[3])
+        {
+            connected = true;
+            break;
+        }
+    }
+}
+
+void CreateGraph2()
+{
+
+    // 연결된 목록을 따로 관리
+    // adjacent[n] -> n번 정점과 연결된 정점 목록
+    vector<vector<int>> adjacent(6);
+    adjacent[0] = { 1, 3 };
+    adjacent[1] = { 0, 2, 3 };
+    adjacent[3] = { 4 };
+    adjacent[5] = { 4 };
+
+    
+    // 0번 -> 3번 정점이 연결되어 있는지 확인.
+    bool connected = false;
+    for (int vertex : adjacent[0])
+    {
+        if (vertex == 3)
+        {
+            connected = true;
+            break;
+        }
+    }
+
+    // STL
+    vector<int>& adj = adjacent[0];
+    bool connected2 = (::find(adj.begin(), adj.end(), 3) != adj.end());
+    
+}
+
+void CreateGraph3()
+{
+
+    // 연결된 목록을 따로 관리
+    // [X][O][X][O][X][X]
+    // [O][X][O][O][X][X]
+    // [X][X][X][X][X][X]
+    // [X][X][X][X][O][X]
+    // [X][X][X][X][X][X]
+    // [X][X][X][X][O][X]
+
+    // 읽는 방법 : adjacent[from][to]
+    // 행렬을 이용한 그래프 표현. (2차원 배열)
+    // 메모리 소모가 심하긴 하지만, 빠른 접근 가능.
+    // 간선이 많은 경우 이점이 있다.
+    vector<vector<bool>> adjacent(6, vector<bool>(6, false));
+    adjacent[0][1] = true;
+    adjacent[0][3] = true;
+    adjacent[1][0] = true;
+    adjacent[1][2] = true;
+    adjacent[1][3] = true;
+    adjacent[3][4] = true;
+    adjacent[5][4] = true;
+
+
+    // 0번 -> 3번 정점이 연결되어 있는지 확인.
+    bool connected = adjacent[0][3];
+
+    vector<vector<int>> adjacent2 =
+    {
+        vector<int> { -1, 15, -1, 35, -1, -1 },
+        vector<int> { 15, -1, 5, 10, -1, -1 },
+        vector<int> { -1, -1, -1, -1, -1, -1 },
+        vector<int> { -1, -1, -1, -1, 5, -1 },
+        vector<int> { -1, -1, -1, -1, -1, -1 },
+        vector<int> { -1, -1, -1, -1, 5, -1 },
+    };
+
+    adjacent[0][3];
+    
+}
+
+void CreateGraph4()
+{
+    // 가중치가 추가된 그래프.
+    // 갈수없는 곳은 -1, 갈수있는 곳은 가중치를 넣어줌.
+    vector<vector<int>> adjacent =
+    {
+        vector<int> { -1, 15, -1, 35, -1, -1 },
+        vector<int> { 15, -1, 5, 10, -1, -1 },
+        vector<int> { -1, -1, -1, -1, -1, -1 },
+        vector<int> { -1, -1, -1, -1, 5, -1 },
+        vector<int> { -1, -1, -1, -1, -1, -1 },
+        vector<int> { -1, -1, -1, -1, 5, -1 },
+    };
+
+    // 0번 -> 3번 정점이 연결되어 있는지 확인.
+    if (adjacent[0][3] != -1)
+        cout << adjacent[0][3] << endl;
+
+
+}
+#pragma endregion
+
 
 int main()
 {
-    ArrayQueue<int> q;
-
-    for (int i = 0; i < 100; i++)
-        q.push(i);
-
-    while (q.empty() == false)
-    {
-        int value = q.front();
-        q.pop();
-        cout << value << endl;
-    }
-
-    int size = q.size();
-
-    cout << size << endl;
+    CreateGraph1();
+    CreateGraph2();
+    CreateGraph3();
+    CreateGraph4();
 }
 
