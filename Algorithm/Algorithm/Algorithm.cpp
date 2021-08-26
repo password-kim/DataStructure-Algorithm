bf4055428c6e62c2306a34ceefa161608ec6fef1
password-kim <72890183+password-kim@users.noreply.github.com>
2021-08-26 19:32:13 +0900

    Implement BST(BinarySearchTree)!

--- Algorithm/Algorithm/Algorithm.cpp
+++ Algorithm/Algorithm/Algorithm.cpp
@@ -3,47 +3,30 @@
 #include <list>
 #include <stack>
 #include <queue>
+#include "BinarySearchTree.h"
 using namespace std;
 
-// 이진 탐색(Binary Search)
-// => 정렬된 배열을 이용해서 이진 탐색 가능
-// => 연결 리스트로는 불가(연결 리스트는 임의 접근 불가)
-
-vector<int> numbers;
-
-void BinarySearch(int N)
-{
-	int left = 0;
-	int right = (int)numbers.size() - 1;
-
-	while (left <= right)
-	{
-		cout << "탐색 범위: " << left << " ~ " << right << endl;
-
-		int mid = (left + right) / 2;
-
-		if (N < numbers[mid])
-		{
-			cout << N << " < " << numbers[mid] << endl;
-			right = mid - 1;
-		}
-		else if (N > numbers[mid])
-		{
-			cout << N << " > " << numbers[mid] << endl;
-			left = mid + 1;
-		}
-		else
-		{
-			cout << "찾음!" << endl;
-			break;
-		}
-	}
-}
+// 이진 탐색 트리
+// => 배열로 구현된 이진 탐색은 중간 삽입/삭제가 힘들다.
+// => 따라서 이진 탐색 트리로 보완을 해야한다.
 
 int main()
 {
-	numbers = vector<int>{ 1, 8, 15, 23, 32, 44, 56, 63, 81, 91 };
-	
-	BinarySearch(81);
+	BinarySearchTree bst;
+
+	bst.Insert(20);
+	bst.Insert(10);
+	bst.Insert(30);
+	bst.Insert(40);
+	bst.Insert(50);
+	bst.Insert(25);
+	bst.Insert(26);
+
+	//bst.Print_Inorder();
+
+	bst.Delete(20);
+
+	bst.Print();
+
 }
 
