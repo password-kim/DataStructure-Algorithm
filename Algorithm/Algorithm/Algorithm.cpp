b5971e9851ddca9b54f76528b7ebf5977fad7784
password-kim <72890183+password-kim@users.noreply.github.com>
2021-09-02 17:33:48 +0900

    Basic Sort(Bubble, Seletion, Insertion Sort)

--- Algorithm/Algorithm/Algorithm.cpp
+++ Algorithm/Algorithm/Algorithm.cpp
@@ -4,43 +4,89 @@
 #include <stack>
 #include <queue>
 #include <thread>
-#include "BinarySearchTree.h"
 using namespace std;
 
-// 이진 탐색 트리
-// => 배열로 구현된 이진 탐색은 중간 삽입/삭제가 힘들다.
-// => 따라서 이진 탐색 트리로 보완을 해야한다.
+// 정렬
+
+// 1) 버블 정렬 (Bubble Sort)
+void BubbleSort(vector<int>& v)
+{
+	const int n = (int)v.size();
+
+	// (N - 1) + (N - 2) + ... + 2 + 1
+	// => N * (N - 1) / 2
+	// => O(N^2)
+	for (int i = 0; i < n - 1; i++)
+	{
+		for (int j = 0; j < (n - 1 - i); j++)
+		{
+			if (v[j] > v[j + 1])
+			{
+				//swap(v[j], v[j + 1]);
+				int temp = v[j];
+				v[j] = v[j + 1];
+				v[j + 1] = temp; 
+			}
+		}
+	}
+}
+
+// 2) 선택 정렬 (Selection Sort)
+void SelectionSort(vector<int>& v)
+{
+	const int n = (int)v.size();
+
+	// => O(N^2)
+	for (int i = 0; i < n - 1; i++)
+	{
+		int bestIdx = i;
+
+		for (int j = i + 1; j < n; j++)
+		{
+			if (v[j] < v[bestIdx])
+				bestIdx = j; 
+		}
+
+		// swap
+		int temp = v[i];
+		v[i] = v[bestIdx];
+		v[bestIdx] = temp;
+
+	}
+}
+
+// 3) 삽입 정렬 (Insertion Sort)
+void InsertionSort(vector<int>& v)
+{
+	const int n = (int)v.size();
+
+	// => O(N^2)
+	for (int i = 1; i < n; i++)
+	{
+		int insertData = v[i];
+
+		int j;
+
+		for (j = i - 1; j >= 0; j--)
+		{
+			if (v[j] > insertData)
+				v[j + 1] = v[j];
+			else
+				break;
+		}
+
+		v[j + 1] = insertData;
+	}
+}
 
 int main()
 {
-	BinarySearchTree bst;
+	vector<int> v = { 2, 4, 5, 1, 3 };
 
-	bst.Insert(30);
-	bst.Print();
-	this_thread::sleep_for(1s);
+	//BubbleSort(v);
+	//SelectionSort(v);
+	InsertionSort(v);
 
-	bst.Insert(10);
-	bst.Print();
-	this_thread::sleep_for(1s);
-
-	bst.Insert(20);
-	bst.Print();
-	this_thread::sleep_for(1s);
-
-	bst.Insert(25);
-	bst.Print();
-	this_thread::sleep_for(1s);
-
-	bst.Delete(20);
-	bst.Print();
-	this_thread::sleep_for(1s);
-
-	bst.Delete(10);
-	bst.Print();
-	this_thread::sleep_for(1s);
-	
-
-	//bst.Print_Inorder();
 
 }
 
