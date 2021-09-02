37403b850bbabe975c1b645ae7ae78dbe189edf8
password-kim <72890183+password-kim@users.noreply.github.com>
2021-09-02 18:58:40 +0900

    Implement QuickSort!

--- Algorithm/Algorithm/Algorithm.cpp
+++ Algorithm/Algorithm/Algorithm.cpp
@@ -6,89 +6,40 @@
 #include <thread>
 using namespace std;
 
-// 힙 정렬
-void HeapSort(vector<int>& v)
+int Partition(vector<int>& v, int left, int right)
 {
-	priority_queue<int, vector<int>, greater<int>> pq;
+	int pivot = v[left];
+	int low = left + 1;
+	int high = right;
 
-	// O(NlogN)
-	for (int num : v)
-		pq.push(num);
-
-	v.clear();
-
-	// O(NlogN)
-	while (pq.empty() == false)
+	// O(N)
+	while (low <= high)
 	{
-		v.push_back(pq.top());
-		pq.pop();
+		while (low <= right && pivot >= v[low])
+			low++;
+
+		while (high >= left + 1 && pivot <= v[high])
+			high--;
+
+		if (low < high)
+			swap(v[low], v[high]);
 	}
+
+	swap(v[left], v[high]);
+
+	return high;
 }
 
-// 병합 정렬
-// 분할 정복 (Divide and Conquer)
-// - 분할 (Divide)		문제를 분할하여 정리
-// - 정복 (Conquer)		분할된 문제를 해결
-// - 결합 (Combine)		결과를 취합하여 마무리
-void MergeResult(vector<int>& v, int left, int mid, int right)
+// O(N^2)	: 최악의 경우
+// O(NlogN) : 평균
+void QuickSort(vector<int>& v, int left, int right)
 {
-	int leftIdx = left;
-	int rightIdx = mid + 1;
-
-	vector<int> temp;
-
-	while (leftIdx <= mid && rightIdx <= right)
-	{
-		if (v[leftIdx] <= v[rightIdx])
-		{
-			temp.push_back(v[leftIdx]);
-			leftIdx++;
-		}
-		else
-		{
-			temp.push_back(v[rightIdx]);
-			rightIdx++;
-		}
-	}
-
-
-	// 왼쪽이 먼저 끝났으면, 오른쪽 나머지 데이터 복사
-	if (leftIdx > mid)
-	{
-		while (rightIdx <= right)
-		{
-			temp.push_back(v[rightIdx]);
-			rightIdx++;
-		}
-	}
-	// 오른쪽이 먼저 끝났으면, 왼쪽 나머지 데이터 복사
-	else
-	{
-		while (leftIdx <= mid)
-		{
-			temp.push_back(v[leftIdx]);
-			leftIdx++;
-		}
-		
-	}
-
-	for (int i = 0; i < temp.size(); i++)
-	{
-		v[left + i] = temp[i];
-	}
-}
-
-void MergeSort(vector<int>& v, int left, int right)
-{
-	if (left >= right)
+	if (left > right)
 		return;
 
-	int mid = (left + right) / 2;
-
-	MergeSort(v, left, mid);
-	MergeSort(v, mid + 1, right);
-
-	MergeResult(v, left, mid, right);
+	int pivot = Partition(v, left, right);
+	QuickSort(v, left, pivot - 1);
+	QuickSort(v, pivot + 1, right);
 }
 
 int main()
@@ -102,8 +53,8 @@ int main()
 		int randValue = rand() % 100;
 		v.push_back(randValue);
 	}
-
-	//HeapSort(v);
-	MergeSort(v, 0, v.size() - 1);
+	
+	QuickSort(v, 0, v.size() - 1);
+	
 }
 
