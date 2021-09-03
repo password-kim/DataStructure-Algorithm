992e522b90c73c11ac1d7a9dbb9f2adbea57c6ce
password-kim <72890183+password-kim@users.noreply.github.com>
2021-09-03 16:11:41 +0900

    What is Hash Table?

--- Algorithm/Algorithm/Algorithm.cpp
+++ Algorithm/Algorithm/Algorithm.cpp
@@ -6,55 +6,114 @@
 #include <thread>
 using namespace std;
 
-int Partition(vector<int>& v, int left, int right)
+// 해시 테이블
+
+// map vs hash_map (C++11 표준 unordered_map)
+// - map : Red-Black Tree구조.
+//	- 추가/탐색/삭제 O(logN)
+// - hash_map (unordered_map)
+//	- 추가/탐색/삭제 O(1)
+//	- 메모리를 내주고 속도를 취한다.
+
+void TestTable()
 {
-	int pivot = v[left];
-	int low = left + 1;
-	int high = right;
-
-	// O(N)
-	while (low <= high)
+	struct User
 	{
-		while (low <= right && pivot >= v[low])
-			low++;
+		int userId = 0; // 1~999
+		string username;
+	};
 
-		while (high >= left + 1 && pivot <= v[high])
-			high--;
+	vector<User> users;
+	users.resize(1000);
 
-		if (low < high)
-			swap(v[low], v[high]);
-	}
+	// 777번 유저 정보 세팅
+	users[777] = User{ 777, "PassWordKim" };
 
-	swap(v[left], v[high]);
+	// 777번 유저 이름은?
+	string name = users[777].username;
+	cout << name << endl;
 
-	return high;
+	// 테이블
+	// 키를 알면, 데이터를 단번에 찾을 수 있다!
+
+	// BUT, 데이터가 많아지면 
+	// 메모리 사용 요구량이 많이지기 때문에 비효율적..
 }
 
-// O(N^2)	: 최악의 경우
-// O(NlogN) : 평균
-void QuickSort(vector<int>& v, int left, int right)
+void TestHash()
 {
-	if (left > right)
-		return;
+	struct User
+	{
+		int userId = 0; // 1~int32_max
+		string username;
+	};
 
-	int pivot = Partition(v, left, right);
-	QuickSort(v, left, pivot - 1);
-	QuickSort(v, pivot + 1, right);
+	//       []
+	// [][][][][][][][]
+	vector<User> users;
+	users.resize(1000);
+
+	const int userId = 123456789;
+	int key = (userId % 1000); // hash < 고유번호
+
+	// 123456789번 유저 정보 세팅
+	users[key] = User{ userId, "PassWordKim" };
+
+	// 123456789번 유저 이름은?
+	User& user = users[key];
+	if (user.userId == userId)
+	{
+		string name = user.username;
+		cout << name << endl;
+	}
+
+	// 충돌 문제
+	// 충돌이 발생한 자리를 대신해서 다른 빈자리를 찾아나서면 된다
+	// - 선형 조사법 (linear probing)
+	// hash(key)+1 -> hash(key)+2
+	// - 이차 조사법 (quadratic probing)
+	// hash(key)+1^2 -> hash(key)+2^2
+	// - etc..
+
+	// 체이닝
+}
+
+// O(1)
+void TestHashTableChaining()
+{
+	struct User
+	{
+		int userId = 0; // 1~int32_max
+		string username;
+	};
+
+	// [ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ]
+	vector<vector<User>> users;
+	users.resize(1000);
+
+	const int userId = 123456789;
+	int key = (userId % 1000); // hash < 고유번호
+
+	// 123456789번 유저 정보 세팅
+	users[key].push_back(User{ userId, "PassWordKim" });
+	users[789].push_back(User{ 789, "Kun" });
+
+	// 123456789번 유저 이름은?
+	vector<User>& bucket = users[key];
+	for (User& user : bucket)
+	{
+		if (user.userId == userId)
+		{
+			string name = user.username;
+			cout << name << endl;
+		}
+	}
 }
 
 int main()
 {
-	vector<int> v;
-
-	srand(time(0));
-
-	for (int i = 0; i < 50; i++)
-	{
-		int randValue = rand() % 100;
-		v.push_back(randValue);
-	}
-	
-	QuickSort(v, 0, v.size() - 1);
-	
+	//TestTable();
+	//TestHash();
+	TestHashTableChaining();
 }
 
